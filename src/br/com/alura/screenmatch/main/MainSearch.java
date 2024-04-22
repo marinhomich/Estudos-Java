e45b530232260e8bb6a240b37d0d39eb77d776c9
Michel Marinho <marinhomich@gmail.com>
2024-04-22 14:59:33 -0400

    update

--- src/br/com/alura/screenmatch/main/MainSearch.java
+++ src/br/com/alura/screenmatch/main/MainSearch.java
@@ -7,53 +7,74 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
 
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um filme para a busca: ");
-        var busca = leitura.nextLine();
-
-        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=5f7b0e2c";
-        try {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        String busca = "";
+        List<Titulo> titulos = new ArrayList<>();
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).setPrettyPrinting().create();
+        FileWriter escrita = new FileWriter("filmes.json");
 
 
-        String json = response.body();
-        System.out.println(json);
-        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
+        while (!busca.equalsIgnoreCase("sair")) {
 
-            Titulo meuTitulo = new Titulo(meuTituloOmdb);
-            System.out.println(meuTitulo);
-            
+            System.out.println("Digite um filme para a busca: ");
+            busca = leitura.nextLine();
 
-        } catch (NumberFormatException e){
-            System.out.println("Aconteceu um erro: ");
-            System.out.println(e.getMessage());
-        }  catch (IllegalArgumentException e){
-            System.out.println("Aconteceu outro erro: ");
-            System.out.println(e.getMessage());
-        }  catch (ErroDeConversaoException e){
-            System.out.println(e.getMessage());
+            if (busca.equalsIgnoreCase("sair")) {
+                break;
+            }
+
+            String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=5f7b0e2c";
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(endereco))
+                        .build();
+
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+
+                String json = response.body();
+                System.out.println(json);
+
+                TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(meuTituloOmdb);
+
+                Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                System.out.println(meuTitulo);
+
+                titulos.add(meuTitulo);
+
+            } catch (NumberFormatException e) {
+                System.out.println("Aconteceu um erro: ");
+                System.out.println(e.getMessage());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Aconteceu outro erro: ");
+                System.out.println(e.getMessage());
+            } catch (ErroDeConversaoException e) {
+                System.out.println(e.getMessage());
+            }
+
         }
+        System.out.println(titulos);
 
+        escrita.write(gson.toJson(titulos));
+        escrita.close();
+        System.out.println("O programa finalizou corretamente");
 
-        // Titulo meuTitulo = gson.fromJson(json, Titulo.class);
 
     }
 }
